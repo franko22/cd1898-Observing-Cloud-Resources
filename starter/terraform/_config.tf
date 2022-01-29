03edc3b357c6d2d3da2a20ca7098740d1fd3fc53
franko22 <frank.a@udaan.com>
2022-01-29 11:31:23 +0100

    reconfigured

--- starter/terraform/_config.tf
+++ starter/terraform/_config.tf
@@ -1,6 +1,6 @@
 terraform {
    backend "s3" {
-     bucket = "udacity-course-ami-image"
+     bucket = "udacity-terraform-course-frank"
      key    = "terraform/terraform.tfstate"
      region = "us-east-2"
    }
